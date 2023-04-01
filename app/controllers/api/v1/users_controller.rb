296b677983e678ae44b042b1754f73651b864ceb
TNK-2 <yuuu1993g@gmail.com>
2023-04-01 23:43:28 +0900

    show user api with auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,12 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :authorize_request, only: [:show]
+
+      def show
+        render json: { email: @current_user.email, created_at: @current_user.created_at, updated_at: @current_user.updated_at }
+      end
+
       def create
         user = User.new(user_params)
         if user.save
@@ -15,6 +21,20 @@ module Api
       def user_params
         params.require(:user).permit(:email, :password, :password_confirmation)
       end
+
+      def authorize_request
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        begin
+          @decoded = JsonWebToken.decode(header)
+          @current_user = User.find(@decoded[:user_id])
+        rescue ActiveRecord::RecordNotFound => e
+          render json: { errors: e.message }, status: :unauthorized
+        rescue JWT::DecodeError => e
+          render json: { errors: e.message }, status: :unauthorized
+        end
+      end
+
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get  '/user', to: 'users#show'
       post '/signup', to: 'users#create'
       post '/login', to: 'sessions#create'
     end
