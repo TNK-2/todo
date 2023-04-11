82ad0c5012728207c0fe02c89a4a3a5be9ac7553
TNK-2 <yuuu1993g@gmail.com>
2023-04-11 17:17:25 +0900

    ログイン処理

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoginForm from './LoginForm';
 import SignupForm from './SignupForm';
 
 function App() {
+  const [isLoginForm, setIsLoginForm] = useState(true);
+
+  const handleSignupClick = () => {
+    setIsLoginForm(false);
+  };
+
+  const handleLoginClick = () => {
+    setIsLoginForm(true);
+  };
+
   return (
     <div>
       <h1>Welcome to my app</h1>
-      <SignupForm />
+      {isLoginForm ? (
+        <>
+          <LoginForm />
+          <p>
+            Don't have an account yet?{' '}
+            <button onClick={handleSignupClick}>Signup</button>
+          </p>
+        </>
+      ) : (
+        <>
+          <SignupForm />
+          <p>
+            Already have an account?{' '}
+            <button onClick={handleLoginClick}>Login</button>
+          </p>
+        </>
+      )}
     </div>
   );
 }
