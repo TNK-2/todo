25ab35be2714269e6e12ec47f637a15be8d841f3
TNK-2 <yuuu1993g@gmail.com>
2023-04-19 14:03:30 +0900

    認可処理の切り出し

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -49,17 +49,4 @@ class Api::V1::TodosController < ApplicationController
       @todo = @current_user.todos.find(params[:id])
     end
 
-    def authorize_request
-      header = request.headers['Authorization']
-      header = header.split(' ').last if header
-      begin
-        @decoded = JsonWebToken.decode(header)
-        @current_user = User.find(@decoded[:user_id])
-      rescue ActiveRecord::RecordNotFound => e
-        render json: { errors: e.message }, status: :unauthorized
-      rescue JWT::DecodeError => e
-        render json: { errors: e.message }, status: :unauthorized
-      end
-    end
-
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -22,19 +22,6 @@ module Api
         params.require(:user).permit(:email, :password, :password_confirmation)
       end
 
-      def authorize_request
-        header = request.headers['Authorization']
-        header = header.split(' ').last if header
-        begin
-          @decoded = JsonWebToken.decode(header)
-          @current_user = User.find(@decoded[:user_id])
-        rescue ActiveRecord::RecordNotFound => e
-          render json: { errors: e.message }, status: :unauthorized
-        rescue JWT::DecodeError => e
-          render json: { errors: e.message }, status: :unauthorized
-        end
-      end
-
     end
   end
 end
